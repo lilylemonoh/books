019487cd625ccc443edbfc648aa0d5d58aa02980
lilylemonoh <lilylemonoh@naver.com>
2024-05-15 14:38:22 +0900

    init

---
+++ spring-intro-java-oop/src/main/java/io/github/lilylemonoh/springintrojavaoop/SpringIntroJavaOopApplication.java
@@ -0,0 +1,13 @@
+package io.github.lilylemonoh.springintrojavaoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroJavaOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroJavaOopApplication.class, args);
+	}
+
+}
---
+++ spring-intro-java-oop/src/test/java/io/github/lilylemonoh/springintrojavaoop/SpringIntroJavaOopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lilylemonoh.springintrojavaoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroJavaOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
