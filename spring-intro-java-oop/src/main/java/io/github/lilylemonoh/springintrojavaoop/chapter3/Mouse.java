99e36d3576a469a6671df3efa4b14526b94641a7
yeonsuoh <lilylemonoh@naver.com>
2024-06-10 13:48:27 +0900

    mac commit test

---
+++ spring-intro-java-oop/src/main/java/io/github/lilylemonoh/springintrojavaoop/chapter3/Mouse.java
@@ -0,0 +1,9 @@
+package io.github.lilylemonoh.springintrojavaoop.chapter3;
+
+public class Mouse {
+    public String name;
+    public int age;
+    public int countOfTail;
+
+    public void sing(){}
+}
